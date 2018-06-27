b3118def92568991961c39d146d2e2999acb71c5
Adam Doquiatan <adamdoq@gmail.com>
2018-06-27 14:55:40 -0700

    Lecture 145

--- notes-app/src/index.js
+++ notes-app/src/index.js
@@ -1,5 +1,33 @@
-import { getNotes, createNote } from './notes.js'
+import { createNote } from './notes.js'
+import { setFilters } from './filters.js'
+import { renderNotes } from './views'
 
-console.log(getNotes())
-createNote()
-console.log(getNotes())
+renderNotes()
+
+document.querySelector('#create-note').addEventListener('click', (e) => {
+    const id = createNote()
+    location.assign(`/edit.html#${id}`)
+})
+
+document.querySelector('#search-text').addEventListener('input', (e) => {
+    filters.searchText = e.target.value
+    setFilters({
+        searchText: e.target.value
+    })
+    renderNotes()
+})
+
+document.querySelector('#filter-by').addEventListener('change', (e) => {
+    filters.sortBy = e.target.value
+    setFilters({
+        sortBy: e.target.value
+    })
+    renderNotes()
+})
+
+window.addEventListener('storage', (e) => {
+    if (e.key === 'notes') {
+        renderNotes()
+    }
+})
+
